0f51023f2da92b80888f589d23cc00c146855aec
Hardik Arora <hardikarora826@gmail.com>
2024-06-26 01:54:08 +0530

    pdf render

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -19,10 +19,12 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import SimpleBar from "simplebar-react"
 import PdfFullscreen from './PdfFullscreen';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 interface PdfRendererProps{
   url: string
