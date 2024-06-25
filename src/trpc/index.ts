2bfb49f03096dc3d98d5451ef2e93137bbcf29e3
Hardik Arora <hardikarora826@gmail.com>
2024-06-26 02:48:10 +0530

    lint error

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -12,7 +12,7 @@ export const appRouter = router({
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+    if (!user?.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
     
     // checking if user is in database
     const dbUser= await db.user.findFirst({
