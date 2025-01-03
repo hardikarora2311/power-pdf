439e4da58d70d63f030f52a9f257dc36a79a83b2
Hardik Arora <70485045+hardikarora2311@users.noreply.github.com>
2025-01-03 23:05:46 +0530

    github link removal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import UserAccountNav from "./UserAccountNav";
 import MobileNav from "./MobileNav";
@@ -9,58 +13,80 @@ import { SOURCE_CODE } from "@/config/links";
 import Image from "next/image";
 
 const Navbar = async () => {
-
-
-
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-        <MaxWidthWrapper>
-            <div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
-                <Link href="/" className="flex z-40 font-semibold ">
-                    <span>PowerPDF</span>
-                </Link>
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
+          <Link href="/" className="flex z-40 font-semibold ">
+            <span>PowerPDF</span>
+          </Link>
 
-                <div className="flex items-center justify-center space-x-4">
+          <div className="flex items-center justify-center space-x-4">
+            <MobileNav isAuth={!!user} />
 
-                <MobileNav isAuth={!!user}/>
+            <div className="hidden sm:flex">
+              {!user ? (
+                <>
+                  <Link
+                    href="/pricing"
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Pricing
+                  </Link>
 
-                <div className="hidden sm:flex">
-                    { !user ? <>
-                        <Link href="/pricing" className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm"
-                        })}>Pricing</Link>
+                  <LoginLink
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Sign in
+                  </LoginLink>
 
-                        <LoginLink className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm"
-                        })}>Sign in</LoginLink>
-            
-                        <RegisterLink className={buttonVariants({
-                            size: "sm"
-                        })}>Get Started <ArrowRight className="ml-1.5 h-5 w-5"/>
-                        </RegisterLink>
-            
-                    </> : <>
-                    <Link href="/dashboard" className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm"
-                        })}>Dashboard</Link>
+                  <RegisterLink
+                    className={buttonVariants({
+                      size: "sm",
+                    })}
+                  >
+                    Get Started <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </RegisterLink>
+                </>
+              ) : (
+                <>
+                  <Link
+                    href="/dashboard"
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Dashboard
+                  </Link>
 
-                        <UserAccountNav email={user.email ?? ""} imageUrl={user.picture ?? ''} name={
-                            !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
-                        }/>
-                    </>}
-                </div>
-
-                <Link href={SOURCE_CODE} target="_blank" rel='noreferrer noopener'>
-                    <Image src="/github.svg" height={25} width={25} alt="Github"/>
-                </Link>
-            </div>   
+                  <UserAccountNav
+                    email={user.email ?? ""}
+                    imageUrl={user.picture ?? ""}
+                    name={
+                      !user.given_name || !user.family_name
+                        ? "Your Account"
+                        : `${user.given_name} ${user.family_name}`
+                    }
+                  />
+                </>
+              )}
             </div>
-        </MaxWidthWrapper>
+
+            {/* <Link href={SOURCE_CODE} target="_blank" rel='noreferrer noopener'>
+                    <Image src="/github.svg" height={25} width={25} alt="Github"/>
+                </Link> */}
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </nav>
   );
 };
